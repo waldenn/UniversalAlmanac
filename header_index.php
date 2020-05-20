225589d0b39be4db5c918cb252102d2e48d0dffc
jama <jama@wikischool.org>
2020-05-20 12:27:17 +0200

    tryout

---
+++ header_index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Universal Almanac</title>
+        <link rel="stylesheet" href="css/bootstrap-table.min.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap-table.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+    </head>
+    <body style="background: #fbfaf9; font-family: 'Quicksand', sans-serif;">
+        <div style="margin-left:10%;margin-right:10%">
+            <!--div class="main-title">
+                Universal Almanac <sup id="super_title">with Wikidata</sup>
+            </div-->
